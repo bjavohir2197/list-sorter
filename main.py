da5e265633f84fbdb45bc13b0df1f8535efade60
bjavohir2197 <bjavohir2197@gmail.com>
2026-04-18 15:22:09 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,66 @@
+class Ro'yxat:
+    def __init__(self):
+        self.massiv = []
+
+    def qo'sh(self, element):
+        self.massiv.append(element)
+
+    def sort(self):
+        self.massiv.sort()
+
+    def chiqar(self):
+        return self.massiv
+
+ro'yxat = Ro'yxat()
+ro'yxat.qo'sh(5)
+ro'yxat.qo'sh(2)
+ro'yxat.qo'sh(8)
+ro'yxat.qo'sh(1)
+ro'yxat.sort()
+print(ro'yxat.chiqar())
+```
+
+```python
+class Ro'yxat:
+    def __init__(self):
+        self.massiv = []
+
+    def qo'sh(self, element):
+        self.massiv.append(element)
+
+    def sort(self):
+        self.massiv.sort()
+
+    def chiqar(self):
+        return self.massiv
+
+ro'yxat = Ro'yxat()
+ro'yxat.qo'sh(5)
+ro'yxat.qo'sh(2)
+ro'yxat.qo'sh(8)
+ro'yxat.qo'sh(1)
+ro'yxat.sort()
+print(ro'yxat.chiqar())
+``]
+
+```python
+class Ro'yxat:
+    def __init__(self):
+        self.massiv = []
+
+    def qo'sh(self, element):
+        self.massiv.append(element)
+
+    def sort(self):
+        self.massiv.sort()
+
+    def chiqar(self):
+        return self.massiv
+
+ro'yxat = Ro'yxat()
+ro'yxat.qo'sh(5)
+ro'yxat.qo'sh(2)
+ro'yxat.qo'sh(8)
+ro'yxat.qo'sh(1)
+ro'yxat.sort()
+print(ro'yxat.chiqar())
